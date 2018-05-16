898a8e852a5d49941944560c809b3960d57ecd95
pengjunwei <pengjunwei@meituan.com>
2018-05-16 13:56:37 +0800

    测试OkHttp3

---
+++ app/src/main/java/com/pengjunwei/android/custom/demo/network/okhttp/TestOkHttpActivity.java
@@ -0,0 +1,12 @@
+package com.pengjunwei.android.custom.demo.network.okhttp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestOkHttpActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
